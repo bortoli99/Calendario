9d13f10cfafdf51369ff176922dfa6f59804eb78
Bruno Oliveira Bortoli <brunobortoli@lampp-it.com.br>
2021-09-01 08:51:37 -0500

    Alteração

--- src/main/java/br/goc/ac/sefaz/calendario/config/DataSourceConfig.java
+++ src/main/java/br/goc/ac/sefaz/calendario/config/DataSourceConfig.java
@@ -3,34 +3,15 @@ package br.goc.ac.sefaz.calendario.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 
 @Configuration
 public class DataSourceConfig {
-    
-    @Autowired
-    Environment env;
-
-    // @Bean
-    // @Primary
-    // public DataSource springDataSource(){
-    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    
-    //     dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-    //     dataSource.setUrl(env.getProperty("spring.datasource.url"));
-    //     dataSource.setUsername(env.getProperty("spring.datasource.username"));
-    //     dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        
-
-    //     return dataSource;
-
-    // }
 
     @Bean
     @Primary
